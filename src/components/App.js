68411879b8b5ec4a8e8e6cc5439fa0cc2b27214c
Aran Diez <99818791+AranDiez@users.noreply.github.com>
2022-05-04 20:09:26 +0200

    received data from API and useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
+import { useState, useEffect } from 'react';
 import '../styles/App.scss';
+import getApiData from '../services/moviesApi';
 
 const App = () => {
+  const [dataMovies, setDataMovies] = useState([]);
+
+  useEffect(() => {
+    getApiData().then((dataClean) => {
+      setDataMovies(dataClean);
+    });
+  }, []);
   return (
     <div>
       <p>Hola</p>
@@ -9,3 +18,9 @@ const App = () => {
 };
 
 export default App;
+
+// 1- moviesApi.js
+// 2- importo el array obtenido de la api (getApiData)
+// 3 - Comienzo a hacer la petición al servidor cuando carga la página. Los guardo en la variable dataMovies.
+// 4 - Necesito useEffect para que haga la petición al iniciar página.
+// 5 - Creo el componente movieSceneList para la lista de pelis y movieSceneDetal para cada peli
